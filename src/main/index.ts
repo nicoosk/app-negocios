6b96a4d70b8e3694c87ba2d3740d61fe52f5fd14
Nicolas Huitrayao <nicolas.hui18@gmail.com>
2026-05-23 00:29:05 -0400

    add: Añadiendo ícono de app

--- src/main/index.ts
+++ src/main/index.ts
@@ -91,6 +91,7 @@ function createWindow(): void {
     height: 670,
     show: false,
     autoHideMenuBar: true,
+    title: 'Mi Almacén Digital',
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
